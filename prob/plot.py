cd39348e9465c57e52dbff1b5a7725a8636429cd
Johannes Ihle <johannes.s.ihle@gmail.com>
2023-07-24 13:08:09 +0200

    Added to plot dist

--- prob/plot.py
+++ prob/plot.py
@@ -1,6 +1,27 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import sys
 
+def plot_data(filename, plot_type='hist'):
+    # Read data from file into pandas DataFrame
+    data = pd.read_csv(filename, header=None)
 
-fig, ax = plt.subplots()
+    # Create figure and axis
+    fig, ax = plt.subplots()
 
-file = 
+    # Plot data
+    if plot_type == 'hist':
+        sns.histplot(data[0], kde=False, ax=ax)
+    elif plot_type == 'kde':
+        sns.kdeplot(data[0], ax=ax)
+    else:
+        print(f'Unknown plot type: {plot_type}')
+        sys.exit(1)
+
+    # Display plot
+    plt.show()
+
+# Use the function
+plot_data('yourfile.txt', 'hist')  # change 'yourfile.txt' to your actual file name
+# For KDE plot, use: plot_data('yourfile.txt', 'kde')
