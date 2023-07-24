0f3e72fb2047e41178378635b43c07eec304b84d
Johannes <johannes.s.ihle@gmail.com>
2023-07-24 12:56:28 +0200

    Added the following
    * Cool iterator
    * Possibly a mixture exponential pdf generator. Remains to be tested.

--- cool_iterating/src/main.c
+++ cool_iterating/src/main.c
@@ -4,10 +4,25 @@
 
 int main(void) {
 
+	//For example iterate through pairs of verteces thus 
+	//iterating through edges (i, j)
 	int nvert = 10; 
 	for (int i = 0, j = nvert-1; i < nvert; j = i++){
 		printf("(i, j) = (%i, %i)\n", i, j); 
 	}
+
+	int n = 10; //assume >=3
+
+	for (int i=0, j=(i+1)%n, k=(i+2)%n; i < n; i=i+1, j=(j+1)%n, k=(k+1)%n){
+
+
+		printf("(i, j, k) = (%i, %i, %i)\n", i, j, k); 
+
+		// i+=1; 
+		// j=(j+1)%n; 
+		// k=(k+1)%n; 
+	}
+
 
 	return 0; 
 }
---
+++ prob/src/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <math.h>
+
+float F(float l, float l_min, float l1, float l2, float q){
+	float P = (q*exp(-(l-l_min)/l1) + (1-q)*exp(-(l-l_min)/l2)); //P as per P(l) according to paper eq (1)
+	return 1-P; 
+}
+
+float gen_mixt_expo_value(float l_min, float l1, float l2, float q){
+
+	float randNum = (float)rand() / RAND_MAX; 
+
+	float min_value = l_min; 
+	float max_value = 500.0; 
+
+	float left = min_value; 
+	float right = max_value; 
+
+	float epsilon = 1.0; 
+
+	while (right - left > epsilon){
+		float mid = (left+right)/2; 
+		if (F(mid, l_min, l1, l2, q) > randNum) {
+			right = mid; 
+		}else{
+			left = mid; 
+		}
+	}
+
+	return (left + right)/2; 
+}
+
+int main(void) {
+
+	srand(0); 
+
+	FILE *file = fopen("random_nums.txt", "w"); 
+	if (file == NULL){
+		printf("Error opening file\n"); 
+		return 1; 
+	}
+
+	for (int i = 0; i < 1000; i++)
+	{
+		float x = gen_mixt_expo_value(5.0, 17.23, 82.36, 0.44); 
+		fprintf(file, "%f\n", x); 
+	}
+	
+	fclose(file); 
+
+	return 0; 
+}
